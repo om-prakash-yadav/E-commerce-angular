15eb8809d116904d4a4ad57d07b4a67b81a33d08
s-abhimanyu <masterabhimanyustar@gmail.com>
2024-08-01 16:07:36 +0530

    Header Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartsComponent } from './carts/carts.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule,Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'carts',component:CartsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent},
+  {path:'login',component:LoginComponent},
+  {path:'not-found',component:NotFoundComponent},
+  {path:'**',redirectTo:'not-found'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartsComponent,
+    ContactComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
